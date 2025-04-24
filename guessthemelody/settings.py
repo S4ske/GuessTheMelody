d8cc8faee0e0595fd815583845654520126fc2e2
S4ske <sany1111.ru@mail.ru>
2025-04-25 00:37:33 +0500

    Разрешил все хосты 4

--- guessthemelody/settings.py
+++ guessthemelody/settings.py
@@ -62,8 +62,7 @@ MIDDLEWARE = [
 	'django.contrib.messages.middleware.MessageMiddleware',
 	'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-CORS_ALLOWED_ORIGINS = ['*']
-CORS_ALLOW_METHODS = ['*']
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 
 
