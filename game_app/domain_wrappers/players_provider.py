579f43cb0cc335620ee40bc42dd7839769aa1961
S4ske <sany1111.ru@mail.ru>
2025-05-09 16:08:10 +0500

    Добавил проверку на мастера в state_info_provider

--- game_app/domain_wrappers/players_provider.py
+++ game_app/domain_wrappers/players_provider.py
@@ -11,31 +11,31 @@ class PlayersProvider(PlayersProviderABC):
 
     def get_all_players(self) -> list[PlayerDTO]:
         return [PlayerDTO(nickname=player.nickname, points=player.points, is_master=player.is_master)
-                for player in Player.objects.filter(game__pk=self._game_id).all()]
+                for player in Player.objects.filter(game__pk=self._game_id, is_master=False).all()]
 
     @property
     def players_count(self) -> int:
-        return Player.objects.filter(game__pk=self._game_id).count()
+        return Player.objects.filter(game__pk=self._game_id, is_master=False).count()
 
     def add_points(self, nickname: str, points: int) -> None:
-        player = Player.objects.get(game__pk=self._game_id, nickname=nickname)
+        player = Player.objects.get(game__pk=self._game_id, nickname=nickname, is_master=False)
         player.points += points
         player.save()
 
     def remove_points(self, nickname: str, points: int) -> None:
-        player = Player.objects.get(game__pk=self._game_id, nickname=nickname)
+        player = Player.objects.get(game__pk=self._game_id, nickname=nickname, is_master=False)
         player.points -= points
         player.save()
 
     def get_random_nickname(self) -> str:
-        players = Player.objects.filter(game__pk=self._game_id)
+        players = Player.objects.filter(game__pk=self._game_id, is_master=False)
         return players[randint(0, len(players) - 1)].nickname
 
     def get_points(self, nickname: str) -> int:
-        return Player.objects.get(game__pk=self._game_id, nickname=nickname).points
+        return Player.objects.get(game__pk=self._game_id, nickname=nickname, is_master=False).points
 
     def get_player(self, nickname: str) -> PlayerDTO:
-        player = Player.objects.get(game__pk=self._game_id, nickname=nickname)
+        player = Player.objects.get(game__pk=self._game_id, nickname=nickname, is_master=False)
         return PlayerDTO(
             nickname=player.nickname,
             is_master=player.is_master,
